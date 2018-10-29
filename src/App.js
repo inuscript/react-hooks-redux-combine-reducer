a103ec449c9985660efeeb78568d4c7a99a85c7a
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-10-29 17:39:56 +0900

    with context

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
-import React, { Component, useReducer } from "react"
+import React, { Component, useReducer, useContext, createContext } from "react"
 import { rootReducer } from "./reducer"
+
+const ReducerContext = createContext(null)
+
+const Counter = () => {
+  return (
+    <ReducerContext.Consumer>
+      {({ state, dispatch }) => {
+        return (
+          <div>
+            <h1>counter</h1>
+            <div>count: {state.counter}</div>
+            <button onClick={(e) => dispatch({ type: "INCREMENT" })}>+</button>
+            <button onClick={(e) => dispatch({ type: "DECREMENT" })}>-</button>
+          </div>
+        )
+      }}
+    </ReducerContext.Consumer>
+  )
+}
+const InputValue = () => {
+  return (
+    <ReducerContext.Consumer>
+      {({ state, dispatch }) => {
+        return (
+          <div>
+            <h1>Input foo</h1>
+            <div>foo: {state.someNested.inputValue}</div>
+            <input
+              value={state.someNested.inputValue}
+              onChange={(e) =>
+                dispatch({
+                  type: "UPDATE_VALUE",
+                  value: e.target.value
+                })
+              }
+            />
+          </div>
+        )
+      }}
+    </ReducerContext.Consumer>
+  )
+}
 
 const App = () => {
   const [state, dispatch] = useReducer(rootReducer, undefined, {})
-  console.log(state)
 
   return (
-    <div className="App">
-      <div>
-        <h1>counter</h1>
-        <div>count: {state.counter}</div>
-        <button onClick={(e) => dispatch({ type: "INCREMENT" })}>+</button>
-        <button onClick={(e) => dispatch({ type: "DECREMENT" })}>-</button>
+    <ReducerContext.Provider value={{ state, dispatch }}>
+      <div className="App">
+        <Counter />
+        <InputValue />
       </div>
-      <div>
-        <h1>Input foo</h1>
-        <div>foo: {state.someNested.inputValue}</div>
-        <input
-          value={state.someNested.inputValue}
-          onChange={(e) =>
-            dispatch({
-              type: "UPDATE_VALUE",
-              value: e.target.value
-            })
-          }
-        />
-      </div>
-    </div>
+    </ReducerContext.Provider>
   )
 }
 
