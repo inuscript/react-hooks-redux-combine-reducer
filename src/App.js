7e8a686ae3fbea3eda7eb0e71a06c52de910063d
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-10-30 10:58:09 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import React, { Component, useReducer } from "react"
+import React, { Component, useReducer, useCallback } from "react"
 import { rootReducer } from "./reducer"
 
 const App = () => {
   const [state, dispatch] = useReducer(rootReducer, undefined, {
     type: "DUMMY_INIT"
   })
-  console.log(state)
-
+  const increment = useCallback((e) => dispatch({ type: "INCREMENT" }), [
+    dispatch
+  ])
+  const decrement = useCallback((e) => dispatch({ type: "DECREMENT" }), [
+    dispatch
+  ])
+  const updateValue = useCallback(
+    (e) =>
+      dispatch({
+        type: "UPDATE_VALUE",
+        value: e.target.value
+      }),
+    [dispatch]
+  )
   return (
     <div className="App">
       <div>
         <h1>counter</h1>
         <div>count: {state.counter}</div>
-        <button onClick={(e) => dispatch({ type: "INCREMENT" })}>+</button>
-        <button onClick={(e) => dispatch({ type: "DECREMENT" })}>-</button>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
       </div>
       <div>
         <h1>Input foo</h1>
         <div>foo: {state.someNested.inputValue}</div>
-        <input
-          value={state.someNested.inputValue}
-          onChange={(e) =>
-            dispatch({
-              type: "UPDATE_VALUE",
-              value: e.target.value
-            })
-          }
-        />
+        <input value={state.someNested.inputValue} onChange={updateValue} />
       </div>
     </div>
   )
